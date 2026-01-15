340f42e608d73364e7033b81ecb8899e685baa2d
Andrew Shen <andrewshenrocks@gmail.com>
2026-01-14 21:29:14 -0800

    Working on problem 2

--- lib/part2.ml
+++ lib/part2.ml
@@ -1,9 +1,21 @@
 open Base
 open Util
 
-let rec compress (equal : 'a -> 'a -> bool) (xs : 'a list) : 'a list = todo ()
+let rec compress (equal : 'a -> 'a -> bool) (xs : 'a list) : 'a list = 
+    match xs with
+    | [] -> []
+    | h::b::t when equal h b -> compress equal (h::t) 
+    | h::t -> h::(compress equal t)
+;;
 
-let max (xs : int list) : int option = todo ()
+let int_max x y = if x > y then x else y;;
+
+let max (xs : int list) : int option = 
+    match xs with
+    | [] -> None
+    | h::t -> int_max h (max t)
+;;
+
 let rec join (xs : 'a option list) : 'a list option = todo ()
 
 let insert (key : 'k) (value : 'v) (dict : ('k * 'v) list) : ('k * 'v) list =
