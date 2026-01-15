0dd92d4151e652b1348bc95b119cffef20e1e9ab
Andrew Shen <andrewshenrocks@gmail.com>
2026-01-15 02:10:40 -0800

    Finished compress and max

--- lib/part2.ml
+++ lib/part2.ml
@@ -4,16 +4,19 @@ open Util
 let rec compress (equal : 'a -> 'a -> bool) (xs : 'a list) : 'a list = 
     match xs with
     | [] -> []
-    | h::b::t when equal h b -> compress equal (h::t) 
+    | a::b::rest when equal a b -> compress equal (a::rest) 
     | h::t -> h::(compress equal t)
 ;;
 
-let int_max x y = if x > y then x else y;;
-
 let max (xs : int list) : int option = 
+    let rec helper_max (curr_max : int) (l : int list) : int = 
+        match l with
+        | [] -> curr_max
+        | h::t -> if h > curr_max then h else (helper_max curr_max t)
+    in
     match xs with
     | [] -> None
-    | h::t -> int_max h (max t)
+    | h::t -> Some (helper_max h t)
 ;;
 
 let rec join (xs : 'a option list) : 'a list option = todo ()
